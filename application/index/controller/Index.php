88c9b11dfb82f8c690c01ee9cdd1c2693ea5c7a9
zhiming <358258296@qq.com>
2019-05-15 16:32:26 +0800

    Signed-off-by: zhiming <358258296@qq.com>

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -7,9 +7,9 @@ class Index extends Controller
     public function index()
     {
 		$result=new MoneyResult();
-	    $result=$result->getAll(1,100);
+	    $result=$result->getAll(1,1000);
 		foreach($result as $k=>$v){
-		 $result[$k]->lstno=(1*100)+$k;
+		 $result[$k]->lstno=(0*1000)+$k+1;
 		 $result[$k]->actdate=str_replace('-','/',$result[$k]->actdate);
 		}
         return view('',["result"=>json_encode($result)],["__PUBLIC__"=>"/moneyresult/public"]);
